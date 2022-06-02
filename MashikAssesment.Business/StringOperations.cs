834af68bbebe27138feb41a8317cc55061a2e106
e_mert <emert@tx3services.com>
2022-06-02 19:49:40 +0300

    Add project files.

---
+++ MashikAssesment.Business/StringOperations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MashikAssesment.Business
+{
+    public static class StringOperations
+    {
+        public static bool ValidateParentheses(string textToValidate)
+        {
+            if (string.IsNullOrEmpty(textToValidate) || string.IsNullOrWhiteSpace(textToValidate))
+                throw new Exception("Invalid input");
+
+            if (textToValidate.IndexOf('(') < 0 && textToValidate.IndexOf(')') < 0)
+                return true; // there is no parentheses pair to validate
+
+            if (textToValidate.IndexOf('(') < 0 || textToValidate.IndexOf(')') < 0)
+                return false; // textToValidate doesn't contain any opening or closing parenthesis
+
+            bool haveAnOpeningParenthesisBeforeClosingParenthesis = textToValidate.IndexOf('(') > textToValidate.IndexOf(')');
+            if (haveAnOpeningParenthesisBeforeClosingParenthesis)
+                return false;
+
+            int numberOfClosingParenthesisWeNeed = 0;
+            foreach (var c in textToValidate)
+            {
+                if (c == '(')
+                    numberOfClosingParenthesisWeNeed++;
+
+                if (c == ')')
+                    numberOfClosingParenthesisWeNeed--;
+
+                if (numberOfClosingParenthesisWeNeed < 0)
+                    return false;
+            }
+
+            return numberOfClosingParenthesisWeNeed == 0;
+        }
+    }
+}
---
+++ MashikAssesment.Tests/StringOperationsTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MashikAssesment.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MashikAssesment.Business.Tests
+{
+    [TestClass()]
+    public class StringOperationsTests
+    {
+        [DataRow("Text without parentheses")]
+        [DataRow("( )")]
+        [DataRow("I have ( and )")]
+        [DataRow("I have () and ()")]
+        [DataRow("I have ( ( and ) )")]
+        [DataTestMethod]
+        public void ValidateParentheses_should_return_true(string input)
+        {
+            Assert.IsTrue(StringOperations.ValidateParentheses(input));
+        }
+
+        [DataRow("(")]
+        [DataRow(")")]
+        [DataRow("())(")]
+        [DataRow("I have ( and ) )")]
+        [DataRow("I have ( ( and ) ")]
+        [DataRow("safasfd)asdfsadf(asdf")]
+        [DataTestMethod]
+        public void ValidateParentheses_should_return_false(string input)
+        {
+            Assert.IsFalse(StringOperations.ValidateParentheses(input));
+        }
+
+        [DataRow("")]
+        [DataRow(" ")]
+        [DataTestMethod]
+        public void ValidateParentheses_should_throw_exception(string input)
+        {
+            Assert.ThrowsException<Exception>(() => StringOperations.ValidateParentheses(input));
+        }
+    }
+}
